d9ea94b9dc4ac8812d2ad2c3f3ba185ff459db78
HDchrom <haroun1999@hotmail.es>
2021-12-08 13:42:17 +0100

    Update NudoController.cs

--- Assets/Scripts/NudoMinigame/NudoController.cs
+++ Assets/Scripts/NudoMinigame/NudoController.cs
@@ -67,7 +67,8 @@ public class NudoController : MonoBehaviour
             }
             else
             {
-                //MINIGAME COMPLETED
+				//MINIGAME COMPLETED
+				Debug.Log("Won Minigame");
             }
         }
 

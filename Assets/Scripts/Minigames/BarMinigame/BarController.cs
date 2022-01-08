7e42bef777e04323100d885846e7d00e338945dd
HDchrom <haroun1999@hotmail.es>
2022-01-08 02:38:31 +0100

    Added the minigame to the rest of the game.

--- Assets/Scripts/Minigames/BarMinigame/BarController.cs
+++ Assets/Scripts/Minigames/BarMinigame/BarController.cs
@@ -15,7 +15,10 @@ public class BarController : MonoBehaviour
     [SerializeField] BeerBottle beerBottle;
     public GameObject selectedBottle;
     public int filledBottles;
-    void Start()
+
+	bool isMinigameFinished;
+
+	void Start()
     {
         filledBottles = 0;
 
@@ -27,19 +30,22 @@ public class BarController : MonoBehaviour
 
     void Update()
     {
-        //transform.position = minigameController.ConvertFromScreenToViewport(minigameCam, minigameRenderer);
-        transform.position = new Vector3 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -5);
+        transform.position = minigameController.ConvertFromScreenToViewport(minigameCam, minigameRenderer);
+        //transform.position = new Vector3 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -5);
 
-        if (filledBottles >= 3)
+        if (filledBottles >= 3 && !isMinigameFinished)
         {
+			isMinigameFinished = true;
+			StartCoroutine(FinishMinigame());
             Debug.Log("Minigame finished");
         }
 
-
         if (Input.GetMouseButtonDown(0))
         {
-            //Vector2 finalPos = minigameController.ConvertFromScreenToViewport(minigameCam, minigameRenderer);
-            Vector2 finalPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 finalPos = minigameController.ConvertFromScreenToViewport(minigameCam, minigameRenderer);
+
+			//Vector2 finalPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
             cursor.position = finalPos;
             RaycastHit2D hit = Physics2D.Raycast(finalPos, Vector2.zero, Mathf.Infinity, layerMask);
 
@@ -55,4 +61,10 @@ public class BarController : MonoBehaviour
             }
         }
     }
+
+	IEnumerator FinishMinigame()
+	{
+		yield return new WaitForSeconds(2f);
+		MinigameEvents.current.UnloadMinigame(0);
+	}
 }
